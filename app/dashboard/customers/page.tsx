93dfcbb40ee070d31bacfde43c3a4732f8dde373
Eddy <limeddy1125@gmail.com>
2024-09-29 05:33:48 +0800

    second cm

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,29 @@
-export default function Page() {
-  return <p>Customers</p>;
+import { Metadata } from "next";
+import Table from "@/app/ui/customers/table";
+import Search from "@/app/ui/search";
+import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string };
+}) {
+  const query = searchParams?.query || "";
+
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <Search placeholder="Search customers..." />
+      <Suspense fallback={<p>Loading..</p>}>
+        <Table query={query} />
+      </Suspense>
+    </div>
+  );
 }
